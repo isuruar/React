8dfab4b4743867121ab41c6833c70a44a9ece57d
Isuru <32997733+isuruar@users.noreply.github.com>
2021-04-24 11:21:42 +0530

    Add files via upload

---
+++ First Component/index.js
@@ -0,0 +1,38 @@
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 				<h1>Hello there!</h1>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// ReactDOM.render(<Hello />, document.getElementById('root'));
+
+
+// class Hello extends React.Component {
+// 	render() {
+// 		return (
+// 		<div>
+// 			<h1>Hello there</h1>
+// 			<h1>My name Isuru</h1>
+// 		</div>
+// 		)
+// 	}
+// }
+
+
+function Hello(){
+	return(
+		<div>
+			<h1>Hello there!</h1>
+			<h1>Hello there!</h1>
+ 			<h1>Hello there!</h1>
+		</div>
+	);
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
